dd611784d0abd74ab24eeb1c00ce95804556132e
wiziix <147393891+wiziix@users.noreply.github.com>
2024-02-20 19:01:41 +0100

    Update zad6.c

--- zad6.c
+++ zad6.c
@@ -8,6 +8,7 @@ enum Move {
     SCISSORS = 3
 };
 
+//metoda zwaracajaca losowa wartosc 1-3, ktora nastepnie wykorzystywana jest do symulacji "ruchu" komputera
 int getRandomMove() {
     return (rand() % (3 - 1 + 1)) + 1;
 }
@@ -16,8 +17,8 @@ int main()
 {
     enum Move userInput = 0;
     int roundCounter = 0, computerInput = 0, computerPoint = 0, userPoint = 0;
-    char * scores[100][2];
-    srand(time(NULL));
+    char * scores[100][2]; //tablica przechowujaca wyniki oraz ruchy w poszczegolnych rundach
+    srand(time(NULL)); //wywolanie funkcji srand(), zeby funkcja rand() nie generowala zawsze tej samej sekwencji liczb
 
     printf(" ###### Welcome to Rock Paper Scissors Game ######\n");
     printf(" ### Rules: \n"
@@ -28,12 +29,16 @@ int main()
         " ### First one to reach 3 points will win. \n");
 
     while (computerPoint < 3 && userPoint < 3) {
-        
+
+        //pobieranie ruchu gracza
         printf("Input your move : ");
         scanf("%d", &userInput);
-        
+
+        //ustalanie wartosci ruchu komputera
         computerInput = getRandomMove();
 
+        //switch case w ktorym nastepuje zestawienie ruchu gracza z ruchem komputera oraz ustalanie wyniku
+        //dodatkowo mozliwosc wyswietlenia wynikow oraz ruchow w posczegolnych rundach
         switch (userInput) {
         case ROCK:
             if (computerInput == PAPER) {
@@ -109,6 +114,7 @@ int main()
 
         }
 
+        //po uzyskaniu 3 punktow przez komputer lub przez gracza gra zostaje zakonczona
         if (computerPoint == 3 || userPoint == 3)
         {
             printf("You got %d points and the Computer got %d points\n", userPoint, computerPoint);
