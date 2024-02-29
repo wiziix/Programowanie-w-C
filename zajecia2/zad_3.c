0d348518e41502e0026698bd8877d35f0d3eb4d5
wiziix <147393891+wiziix@users.noreply.github.com>
2024-02-29 17:05:17 +0100

    Update zad_3.c

--- zajecia2/zad_3.c
+++ zajecia2/zad_3.c
@@ -12,16 +12,20 @@ typedef struct {
     Point p2;
     Point p3;
     Point p4;
-} Rectangle;
+} Rectangle; //struktura Rectangle skladajaca sie z struktur Point
 
+//metoda zmienia wspolrzedne punktow
 void changeCoordinates(Point * p) {
     p->x = 5;
     p->y = 5;
 }
+
+//metoda obliczajaca odleglosc miedzy punktami
 float distanceCalculator(Point p1, Point p2) {
     return (float)sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y),2));
 }
 
+//metoda oblicza pole i obwod prostokata a nastepnie je wypisuje
 void rectangleCalculator(Rectangle r) {
     float a = distanceCalculator(r.p1, r.p2);
     float b = distanceCalculator(r.p3, r.p4);
@@ -30,17 +34,15 @@ void rectangleCalculator(Rectangle r) {
 }
 
 int main() {
-
     Point p1 = { 1,2 };
 
-    changeCoordinates(&p1);
+    changeCoordinates(&p1); 
 
-    printf("(%d,%d)", p1.x, p1.y);
+    printf("(%d,%d)", p1.x, p1.y); 
 
-    Rectangle r1 = { {2,5},{8,5},{2,3},{8,3} };
+    Rectangle r1 = { {2,5},{8,5},{2,3},{8,3} }; 
 
     printf("\n");
 
-    rectangleCalculator(r1);
-
+    rectangleCalculator(r1); 
 }
