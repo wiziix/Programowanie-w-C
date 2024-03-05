aa5e60247f941c6dced0d18a9a7fa319e266225f
wiziix <147393891+wiziix@users.noreply.github.com>
2024-03-05 11:44:39 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,8 +5,8 @@ find . -name '*.c' | while read file; do
     if [ -f "$file" ]; then
         filename=$(basename "$file" .c)
         output_path=$(dirname "$file")
-        clang -o "${output_path}/${filename}" "$file"
-        clang -o "${output_path}/${filename}.exe" "$file"
+        clang -o "${output_path}/${filename}" "$file" -lm
+        clang -o "${output_path}/${filename}.exe" "$file" -lm
         if [ $? -eq 0 ]; then
             echo "Compilation successful for $file"
         else
