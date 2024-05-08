6e86a343887ac5eeea27dcabe521f6a9ea8b665d
wiziix <147393891+wiziix@users.noreply.github.com>
2024-05-08 14:24:16 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -15,3 +15,18 @@ find . -name '*.c' | while read file; do
         fi
     fi
 done
+
+find . -name '*.cpp' | while read file; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file" .cpp)
+        output_path=$(dirname "$file")
+        clang++ -o "${output_path}/${filename}" "$file" 
+        clang++ -o "${output_path}/${filename}.exe" "$file" 
+        if [ $? -eq 0 ]; then
+            echo "Compilation successful for $file"
+        else
+            echo "Error compiling $file"
+            exit 1 # Exit the pipeline with a non-zero status code
+        fi
+    fi
+done
